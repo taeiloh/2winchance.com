0581ea1c30591cd19e64b7d481605689309b4631
박형은 <hepark@idevel.kr>
2022-05-10 22:38:30 +0900

    header active 수정

--- www/common/header.php
+++ www/common/header.php
@@ -1,8 +1,40 @@
+<?php
+//변수 정리
+$_liClass1  = '';
+$_liClass2  = '';
+$_liClass3  = '';
+$_liClass4  = '';
+$_liClass5  = '';
+
+$_url   =   $_SERVER['REQUEST_URI'];
+$_url_f     = explode('/',$_url);
+$_url_f3    = explode('?', $_url_f[1])[0];
+
+switch ($_url_f3) {
+    case 'lobby':
+        $_liClass1  = 'active';
+        break;
+    case 'lineups';
+        $_liClass2  = 'active';
+        break;
+    case 'contests':
+        $_liClass3  = 'active';
+        break;
+    case 'store':
+        $_liClass4  = 'active';
+        break;
+    case 'event':
+        $_liClass5  = 'active';
+        break;
+    default:
+        break;
+}
+?>
 <div class="inner">
     <h1 class="logo"><a href="/main/"><img src="/images/logo.png" alt="METAGAMES"></a></h1>
     <nav id="gnb">
         <ul>
-            <li class="active"><a href="/lobby/">로비</a>
+            <li class="<?=$_liClass1;?>"><a href="/lobby/">로비</a>
                 <ul class="sub-menu">
                     <li class="active"><a href="javascript:void(0)">리그 오브 레전드</a></li>
                     <li><a href="javascript:void(0)">배틀그라운드</a></li>
@@ -10,10 +42,10 @@
                     <li><a href="javascript:void(0)">GS:GO</a></li>
                 </ul>
             </li>
-            <li><a href="/lineups/">LINEUPS</a></li>
-            <li><a href="/contests/">콘텐츠</a></li>
-            <li><a href="/store/">스토어</a></li>
-            <li><a href="/event/">이벤트</a></li>
+            <li class="<?=$_liClass2;?>"><a href="/lineups/">LINEUPS</a></li>
+            <li class="<?=$_liClass3;?>"><a href="/contests/">콘텐츠</a></li>
+            <li class="<?=$_liClass4;?>"><a href="/store/">스토어</a></li>
+            <li class="<?=$_liClass5;?>"><a href="/event/">이벤트</a></li>
         </ul>
     </nav>
     <div class="login-wrap">
