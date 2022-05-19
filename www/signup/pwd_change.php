7aeda7d2e71147d85941b332093bbf7f0c45e844
오태일 <tioh@idevel.kr>
2022-05-19 23:13:26 +0900

    비밀번호 체킹 스크립트 추가

--- www/signup/pwd_change.php
+++ www/signup/pwd_change.php
@@ -19,11 +19,13 @@ try {
     ?>
 </head>
 <body>
+<form id="loginFrm" name="loginFrm" method="post" action="/">
+    <input type="hidden" name="m_idx" id="m_idx" />
 <div id="wrap" class="member">
     <!--content-->
     <div id="content">
         <!--sec-01-->
-        <h1 class="logo"><a href="../html/index.html"><img src="../images/logo.png" alt="METAGAMES"></a></h1>
+        <h1 class="logo"><a href="../main/index.php"><img src="../images/logo.png" alt="METAGAMES"></a></h1>
         <section class="sec sec-01">
             <div class="inner">
                 <div class="title">
@@ -42,20 +44,20 @@ try {
                         <div class="login-box">
                             <div class="input-box">
                                 <label for="">현재 비밀번호</label>
-                                <input type="password" placeholder="현재 비밀번호를 입력해주세요.">
+                                <input type="password" name="now_pw" id="now_pw" placeholder="현재 비밀번호를 입력해주세요.">
                                 <!--<p class="invalid-feedback">영문으로 시작하는 e-mail 주소를 입력해 주세요.</p>-->
                             </div>
                             <div class="input-box">
                                 <label for="">신규 비밀번호</label>
-                                <input type="password" placeholder="비밀번호를 입력해주세요.">
+                                <input type="password" name="m_pw" id="m_pw" placeholder="비밀번호를 입력해주세요.">
                             </div>
                             <div class="input-box">
                                 <label for="">비밀번호 확인</label>
-                                <input type="password" placeholder="비밀번호를 확인해주세요.">
+                                <input type="password" name="m_pw_re" id="m_pw_re" placeholder="비밀번호를 확인해주세요.">
                                 <p class="invalid-feedback">8자리 이상의 영문,숫자,특수문자 조합의 숫자를 입력하세요.</p>
                             </div>
                         </div>
-                        <button type="button" class="btn-blue btn-6">다음</button>
+                        <button type="button" class="btn-blue btn-6" onclick="next()">다음</button>
                     </div>
                 </form>
             </div>
@@ -63,6 +65,7 @@ try {
         <!--//sec-01-->
         <!--//content-->
     </div>
+    </form>
     <footer>
         © 2022 METAGAMES, Inc. All Rights Reserved.
     </footer>
@@ -73,6 +76,49 @@ try {
             'sitekey' : 'your_site_key'
         });
     };
+    function next(){
+
+        if ($.trim($("#now_pw").val()) == "") {
+            alert("현재 비밀번호를 입력해 주세요.");
+            $("#now_pw").focus();
+            return false;
+        }
+
+        if ($.trim($("#m_pw").val()) == "") {
+            alert("신규 비밀번호를 입력해 주세요.");
+            $("#m_pw").focus();
+            return false;
+        }
+        var pw = $("#m_pw").val();
+        var checkNumber = pw.search(/[0-9]/g);
+        var checkEnglish = pw.search(/[a-z]/ig);
+
+        if(pw.length < 8 || pw.length > 20){
+            alert("8자리 ~ 20자리 이내로 입력해주세요.");
+            return false;
+        }else if(!/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/.test(pw)){
+            alert('숫자+영문자+특수문자 조합으로 8자리 이상 사용해야 합니다.');
+            return false;
+        }else if(checkNumber <0 || checkEnglish <0){
+            alert("숫자와 영문자를 혼용하여야 합니다.");
+            return false;
+        }else if(/(\w)\1\1\1/.test(pw)){
+            alert('같은 문자를 4번 이상 사용하실 수 없습니다.');
+            return false;
+        }else {
+            console.log("통과");
+        }
+        if ($.trim($("#m_pw_re").val()) == "") {
+            alert("신규 비밀번호 확인을 입력해 주세요.");
+            $("#m_pw_re").focus();
+            return false
+        }
+        if  (pw != $("#m_pw_re").val()) {
+            alert("비밀번호가 일치하지 않습니다.\n확인 후 다시 입력해 주세요.");
+            $("#m_pw_re").focus();
+            return false;
+        }
+    }
 </script>
 </body>
 </html>
