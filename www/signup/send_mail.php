5b8bfb1d58c13068e7f57b679a6bfe860e818891
안종휘 <jhahn@idevel.kr>
2022-05-25 06:44:19 +0900

    이메일 폼 변경

--- www/signup/send_mail.php
+++ www/signup/send_mail.php
@@ -46,9 +46,142 @@ try {
 
         $title          = '메타게임 회원가입 인증';
         $_querystring   = base64_encode("m_id={$db_id}&key={$pw}");;
-        $_auth_url  = "http://2winchance.com//signup/join_07.php?q=". $_querystring;
+        $_auth_url  = "https://2winchance.com//signup/join_07.php?q=". $_querystring;
 
-        $html       ='<a href="'.$_auth_url.'" target="_blank">메타게임 회원가입 인증</a>';
+        $html       ='<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>METAGAMES</title>
+
+</head>
+<style>
+    @import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css");
+</style>
+<body style="max-width: 620px; margin: 0 auto;">
+<span style="white-space: normal; letter-spacing: normal; text-transform: none; word-spacing: 0px;float:none; color:#ffffff; \ -webkit-text-stroke-width:0px;">
+<table align="center" border="0" cellspacing="0" cellpadding="0"
+       style=" width:100%; max-width: 620px; background-color:#000000; border-top: 8px solid #1e8fff; letter-spacing: -1px">
+    <tbody>
+    <tr>
+        <td align="center">
+            <div style="max-width: 520px; margin:0 auto">
+                <table cellpadding="0" cellspacing="0"
+                       style="width:100%; margin:0 auto; background-color:#000000;  -webkit-text-size-adjust: 100%; text-align: left;">
+                    <tbody>
+                        <tr>
+                            <td colspan="3" height="50"></td>
+                        </tr>
+                        <tr>
+                            <td width="20"></td>
+                            <td>
+                                <table cellpadding="0" cellspacing="0" style="width:100%; margin:0; padding:0;">
+                                    <tbody>
+                                        <tr>
+                                            <td style="margin:0; padding: 0;" valign="top" align="center">
+                                               <a href="tml" target="_blank"><img
+                                                       src="https://syryu.wtest.biz/metagames/html/pc/images/logo_white.svg"
+                                                       alt="METAGAMES" width="224" height="43"
+                                                       style="border:0; margin-right:5px;"></a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="3" height="30"></td>
+                        </tr>
+                        <tr>
+                            <td width="20"></td>
+                            <td style=" background-image: url("https://syryu.wtest.biz/metagames/html/images/img_title_bg.svg"); background-repeat:no-repeat; background-position: center; background-size: contain; height: 123px; line-height: 123px; text-align: center; font-size: 30px; font-weight: 600;  color: #fff;">
+            이메일 인증 완료
+        </td>
+                            <td width="20"></td>
+                        </tr>
+                        <tr>
+                            <td colspan="3" height="30"></td>
+                        </tr>
+                        <tr>
+                            <td width="20"></td>
+                            <td>
+                                <table cellpadding="0" cellspacing="0" style="width:100%; margin:0; padding:0;">
+                                    <tbody>
+                                        <tr>
+
+                                            <td style="vertical-align: top; font-size: 24px;  color: #fff;">
+            안녕하세요! <b
+                                                    style="font-size: 30px; color:#1e8fff; padding: 0 10px; font-weight: bold;">'.$db_id.'</b>님
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                             <td colspan="3" height="40"></td>
+                                        </tr>
+                                         <tr>
+                                            <td style=" color: #ffffff; font-size: 20px; line-height: 30px;">
+            이메일 인증이 완료되었습니다.
+                                                <br>
+        이제 메타게임 계정을 이용하실 수 있습니다.
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                             <td colspan="3" height="50"></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                             <td width="20"></td>
+                        </tr>
+                        <tr>
+                            <td align="center" colspan="3">
+                                <div style="display: inline-block; width: 520px; max-width: 100%; vertical-align: top;">
+                                     <table cellpadding="0" cellspacing="0" style="table-layout: fixed; width: 100%; background:#1e8fff; ">
+                                        <tbody>
+                                            <tr>
+                                                <td height="80" style="text-align: center;">
+                                                     <a href="'.$_auth_url.'" target="_blank" style="display:block; font-size: 35px; text-decoration:none; color:#ffffff; ">닉네임 설정하기</a>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <tr>
+                                 <td colspan="3" height="50"></td>
+                            </tr>
+                            <td align="center" colspan="3">
+                                <div style="display: inline-block; width: 520px; max-width: 100%; vertical-align: top;">
+                                     <table cellpadding="0" cellspacing="0" >
+                                        <tbody>
+                                            <tr>
+                                                <td  style="text-align: center;">
+                                                     <a href="javascript:void(0)" target="_blank" style="display:block; font-size: 12px; text-decoration:none; color:#9f9f9f; ">© 2022 METAGAMES, Inc. All Rights Reserved.</a>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </td>
+                            <tr>
+                                <td colspan="3" height="50"></td>
+                            </tr>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </td>
+    </tr>
+    </tbody>
+</table>
+</span>
+</body>
+</html>
+';
 
 
         //echo $html;
