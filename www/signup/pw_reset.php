787f4915f57266c79af01abc1000712a5c99ae12
오태일 <tioh@idevel.kr>
2022-05-24 14:47:50 +0900

    login -> 비밀번호 찾기 -> 본인인증 -> 비밀번호 재설정 ->예외처리 적용

--- www/signup/pw_reset.php
+++ www/signup/pw_reset.php
@@ -4,6 +4,18 @@ require_once __DIR__ .'/../_inc/config.php';
 checkmobile();
 $idx=!empty($_SESSION['_se_idx']) ? $_SESSION['_se_idx'] : "";      // 세션 시퀀스
 $id=!empty($_SESSION['_se_id']) ? $_SESSION['_se_id'] : "";        // 세션 아이디
+$user_phone = !empty($_POST['userPhone']) ? $_POST['userPhone'] : "";
+
+$query = "SELECT count(*) from members where m_tel = '{$user_phone}'";
+$tresult = mysqli_query($_mysqli,$query);
+$row = mysqli_fetch_row($tresult);
+$total_count = $row[0];
+
+$sql = "SELECT m_id,m_sns_type from members where m_tel = '{$user_phone}'";
+$result1 = $_mysqli->query($sql);
+$memarray = $result1->fetch_array();
+$m_id = $memarray['m_id'];
+$m_sns_type = $memarray['m_sns_type'];
 
 try {
 
@@ -43,21 +55,58 @@ try {
                     <form action="">
                         <div class="sub-content">
                             <h3>본인인증이 완료되었습니다.</h3>
-                            <p class="sub-txt">
-                                비밀번호 재설정을 해주시길 바랍니다.
-                            </p>
-                            <div class="login-box">
-                                <div class="input-box">
-                                    <label for="">신규 비밀번호</label>
-                                    <input type="password" name="m_pw" id="m_pw" placeholder="비밀번호를 입력해주세요.">
+                            <?php
+                            if($total_count > 1 ){?>
+                                <p class="sub-txt">
+                                    귀하의 휴대폰 번호로 가입 된 계정이 <?=$total_count?>개 입니다.</br>
+                                    본 사이트는 1인 1계정만 가입가능 합니다.
+                                    관리자에게 문의바랍니다.
+                                </p>
+                                <div>
                                 </div>
-                                <div class="input-box">
-                                    <label for="">비밀번호 확인</label>
-                                    <input type="password" name="m_pw_re" id="m_pw_re" placeholder="비밀번호를 확인해주세요.">
-                                    <p class="invalid-feedback">8자리 이상의 영문,숫자,특수문자 조합의 숫자를 입력하세요.</p>
+                                <button type="button" class="btn-blue btn-6" onclick="location.href='../main/'">메인으로 돌아가기</button>
+                            <?php
+                            }
+                            else if($total_count == 1)
+                            {
+                                if($m_sns_type == ""){
+                                ?>
+                                <p class="sub-txt">
+                                    귀하의 계정 : <?=$m_id?></br>
+                                    비밀번호 재설정을 해주시길 바랍니다.
+                                </p>
+                                <div class="login-box">
+                                    <div class="input-box">
+                                        <label for="">신규 비밀번호</label>
+                                        <input type="password" name="m_pw" id="m_pw" placeholder="비밀번호를 입력해주세요.">
+                                    </div>
+                                    <div class="input-box">
+                                        <label for="">비밀번호 확인</label>
+                                        <input type="password" name="m_pw_re" id="m_pw_re" placeholder="비밀번호를 확인해주세요.">
+                                        <p class="invalid-feedback">8자리 이상의 영문,숫자,특수문자 조합의 숫자를 입력하세요.</p>
+                                    </div>
                                 </div>
-                            </div>
-                            <button type="button" class="btn-blue btn-6" onclick="next()">다음</button>
+                                <button type="button" class="btn-blue btn-6" onclick="next()">다음</button>
+                                <?php
+                                }else{?>
+                                    <p class="sub-txt">
+                                        귀하는 간편 로그인 계정으로 가입되어있습니다.
+                                        해당 SNS 사이트에서 비밀번호 찾기를 진행하여주십시오.
+                                    </p>
+                                    <div>
+                                    </div>
+                                    <button type="button" class="btn-blue btn-6" onclick="location.href='../main/'">메인으로 돌아가기</button>
+                                <?php
+                                }
+                            }else{?>
+                                <p class="sub-txt">
+                                    귀하의 휴대폰 번호로 가입 된 계정이 없습니다.
+                                </p>
+                                <div>
+                                </div>
+                                <button type="button" class="btn-blue btn-6" onclick="location.href='../main/'">메인으로 돌아가기</button>
+                            <?php
+                            } ?>
                         </div>
                     </form>
                 </div>
