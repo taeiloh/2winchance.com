25963d834e7a8eb46a537714c003b656752985e9
오태일 <tioh@idevel.kr>
2022-05-24 20:00:55 +0900

    remove -> index 변경

--- www/remove/index.php
+++ www/remove/index.php
@@ -9,7 +9,6 @@ try {
     $result = $_mysqli->query($query);
     $_arrMembers = $result->fetch_array();
     $m_id = !empty($_arrMembers['m_id']) ? $_arrMembers['m_id'] : '';
-    $m_name = !empty($_arrMembers['m_name']) ? $_arrMembers['m_name'] : '';
 
 } catch (Exception $e) {
     p($e);
@@ -27,59 +26,55 @@ try {
     require_once __DIR__ .'/../common/head.php';
     ?>
 </head>
-<!--//head-->
+<body>
+<form id="pwdcheckFrm" name="pwdcheckFrm" method="post" action="RemoveAccept.php">
+    <input type="hidden" name="m_idx" id="m_idx" />
+    <div id="wrap" class="member">
 
-<body xmlns="http://www.w3.org/1999/html">
-<div id="wrap" class="member">
-    <!--content-->
-    <div id="content">
-        <!--sec-01-->
-        <h1 class="logo"><a href="../main/index.php"><img src="../images/logo.png" alt="METAGAMES"></a></h1>
-        <section class="sec sec-01">
-            <div class="inner remove-policy">
-                <div class="title">
-                    <h2>회원탈퇴</h2>
-                </div>
-                <div>
-                    <ul class="remove-acct">
-                        <li><a href="javascript:void(0);"><span><?=$m_id?></span></a></li>
-                        <li><a href="javascript:void(0);"><?=$m_name?></a></li>
-                    </ul>
-                    <div class="recent-point">
-                        <h3>잔여포인트</h3>
-                        <div class="user-detail-info">
-                            <ul>
-                                <li><p>캐시</p><span class="fc-yellow coin">54,222</span></li>
-                                <li><p>파이트 포인트</p><span class="fp">16</span></li>
-                                <li><p>명예 포인트</p><span class="hp">16</span></li>
-                            </ul>
+        <!--content-->
+        <div id="content">
+            <!--sec-01-->
+            <h1 class="logo"><a href="../main/index.php"><img src="../images/logo.png" alt="METAGAMES"></a></h1>
+            <section class="sec sec-01">
+                <div class="inner">
+                    <div class="title">
+                        <h2>회원탈퇴</h2>
+                    </div>
+                    <form action="">
+                        <div class="sub-content removeid">
+                            <h3>본인 확인</h3>
+                            <div class="login-box">
+                                <div class="input-box">
+                                    <label>아이디</label>
+                                    <span><?=$m_id?></span>
+                                </div>
+                                <div class="input-box">
+                                    <label for="">비밀번호</label>
+                                    <input type="password" name="u_pw" id="u_pw" placeholder="비밀번호를 확인해주세요.">
+                                    <p class="invalid-feedback">8자리 이상의 영문,숫자,특수문자 조합의 숫자를 입력하세요.</p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                        <button type="button" class="btn-blue btn-6" onclick="next()">다음</button>
+                    </form>
                 </div>
-                <div class="sub-content removeid">
-                    <div>
-                        <p class="checkbox">
-                            <input type="checkbox" class="" id="policy08">
-                            <label for="policy08" class="">회원탈퇴 완료 후 환불이 불가한 것에 대해 인지하였으며 동의합니다.</label>
-                        </p>
-                        <p class="checkbox">
-                            <input type="checkbox" class="" id="policy09" checked="checked">
-                            <label for="policy09" class="">회원탈퇴 시 주의사항에 대해 확인하였으며 회원탈퇴에 동의합니다.</label>
-                        </p>
-                    </div>
-                    <div class="confirm-wrap">
-                        <button type="button" class="btn-red btn-6" onclick="next()">동의 및 본인인증</button>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!--//sec-01-->
-        <!--//content-->
-    </div>
-    <footer>
-        © 2022 METAGAMES, Inc. All Rights Reserved.
-    </footer>
+            </section>
+            <!--//sec-01-->
+            <!--//content-->
+        </div>
+
+        <footer>
+            © 2022 METAGAMES, Inc. All Rights Reserved.
+        </footer>
+</form>
 </div>
+<!--<script type="text/javascript">
+    var onloadCallback = function() {
+        grecaptcha.render('html_element', {
+            'sitekey' : 'your_site_key'
+        });
+    };
+</script>-->
 <script type="text/javascript">
     function next(){
 
