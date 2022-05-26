dae4fe394d7cdc1b27906e332ad355332f2b764c
안종휘 <jhahn@idevel.kr>
2022-05-27 06:19:07 +0900

    로비 링크 오타 수정

--- www/common/lobby_sub_menu.php
+++ www/common/lobby_sub_menu.php
@@ -1,5 +1,5 @@
 <ul class="category inner">
-    <li class="<?=empty($sub_menu)?'active':'';?>"><a href="/lobby/list.php?cate=<?=$cate;?>&g_date=<?=$g_date?>>">ALL</a></li>
+    <li class="<?=empty($sub_menu)?'active':'';?>"><a href="/lobby/list.php?cate=<?=$cate;?>&g_date=<?=$g_date?>">ALL</a></li>
     <li class="<?=($sub_menu==1)?'active':'';?>"><a href="/lobby/list.php?sub_menu=1&cate=<?=$cate;?>&g_date=<?=$g_date?>">FREE ZONE</a></li>
     <li class="<?=($sub_menu==2)?'active':'';?>"><a href="/lobby/list.php?sub_menu=2&cate=<?=$cate;?>&g_date=<?=$g_date?>">TOP 랭킹</a></li>
     <li class="<?=($sub_menu==3)?'active':'';?>"><a href="/lobby/list.php?sub_menu=3&cate=<?=$cate;?>&g_date=<?=$g_date?>">더블 랭킹</a></li>
