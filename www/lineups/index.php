9280f0438c30d4be20218a7b914c033593d8880b
오태일 <tioh@idevel.kr>
2022-05-27 09:52:35 +0900

    라인업 페이지 적용, 잔여연봉, 로고 및 배경 로고 케이스 작업 추가

--- www/lineups/index.php
+++ www/lineups/index.php
@@ -9,6 +9,35 @@ try {
     // 변수 정리
     $where      = '';
 
+    $page = !empty($_GET['page']) ? $_GET['page'] : 1;
+
+    //파라미터 체크
+    if(!is_numeric($page)){
+        $page       =   1;
+    }
+
+    $sql ="select count(*) FROM lineups a 
+                            LEFT JOIN game b
+                                ON a.lu_g_idx = b.g_idx
+                            WHERE 1=1
+                              AND a.lu_u_idx = {$_se_idx}
+                            ORDER BY a.lu_idx DESC";
+    $result11 = mysqli_query($_mysqli, $sql);
+    $row1   = mysqli_fetch_row($result11);
+    $total_count = $row1[0]; //전체갯수
+    $rows = 6;
+    $total_page  = ceil($total_count / $rows);  // 전체 페이지 계산
+    if ($page < 1) { $page = 1; } // 페이지가 없으면 첫 페이지 (1 페이지)
+    $from_record = ($page - 1) * $rows; // 시작 열을 구함
+
+    $dbb = $result11->fetch_assoc();
+
+    $sql2 = "SELECT * FROM lineups a LEFT JOIN game b 
+            ON b.g_idx = a.lu_g_idx LEFT JOIN lineups_history c 
+           ON c.lu_idx = a.lu_idx WHERE 1=1 AND c.lu_idx = {$dbb['lu_idx']}";
+
+    $result12 = $_mysqli->query($sql2);
+
 } catch (Exception $e) {
     p($e);
 }
@@ -52,6 +81,9 @@ try {
                 <div class="inner">
                     <ul class="contest-list lineup-list">
                         <?php
+
+                        if($total_count > 0){
+                            $i=0;
                         $query  = "
                             SELECT a.* 
                             FROM lineups a 
@@ -60,6 +92,7 @@ try {
                             WHERE 1=1
                               AND a.lu_u_idx = {$_se_idx}
                             ORDER BY a.lu_idx DESC
+                            LIMIT {$from_record}, {$rows}
                         ";
                         //p($query);
                         $result = $_mysqli->query($query);
@@ -67,18 +100,57 @@ try {
 
                         }
                         while ($db = $result->fetch_assoc()) {
+                            $i++;
                             //p($db);
+                            $sub_query  = "
+                                SELECT * FROM lineups a
+                                LEFT JOIN game b 
+                                    ON b.g_idx = a.lu_g_idx
+                                LEFT JOIN lineups_history c 
+                                    ON c.lu_idx = a.lu_idx
+                                WHERE 1=1
+                                    AND c.lu_idx = {$db['lu_idx']}
+                            ";
+                            //p($sub_query);
+                            $sub_result = $_mysqli->query($sub_query);
+
+                            $sub_db2 = $sub_result->fetch_assoc();
+
+                            $sub_query2 = "
+                                SELECT 50000-sum(player_salary) as left_salary, b.g_date FROM lineups a
+                                LEFT JOIN game b 
+                                    ON b.g_idx = a.lu_g_idx
+                                LEFT JOIN lineups_history c 
+                                    ON c.lu_idx = a.lu_idx
+                                WHERE 1=1
+                                    AND c.lu_idx = {$db['lu_idx']}
+                            ";
+                            $sub_result2 = $_mysqli->query($sub_query2);
+
+                            $sub_db3 = $sub_result2->fetch_assoc();
+
+                            switch($sub_db2['g_league_alias']){
+                                CASE 'PUBG':
+                                    $img_src = "../images/pubg_logo.png";
+                                    $game_img_src = '../images/img_30multi_50.png';
+                                CASE 'NBA':
+                                    $img_src = "../images/pubg_logo.png";
+                                    $game_img_src = '../images/img_30multi_50.png';
+                            }
 
                             echo <<<LI
                         <li class="edit">
                             <a href="javascript:void(0)" class="active">
-                                <div class="game-thumb" style="background-image: url('../images/@img_thumb01.png')">
-                                    <div class="subject"></div>
+                                <div class="game-thumb" style="background-image: url('$game_img_src')">
+                                    <div class="subject">
+                                    <img src="$img_src" alt="pubg_logo">
+                                    </div>
                                 </div>
                                 <div class="contest-desc lineUP">
                                     <div class="conts-desc-l">
                                         <div class="contest-ico">
-                                            <img src="../images/item2.png" alt="임시">
+                                             <img src="../images/item2.png" alt="임시">
+
                                         </div>
                                         <dl>
                                             <dt class="contest-schedule">내일 경기 예정</dt>
@@ -107,8 +179,8 @@ try {
                                 </thead>
                                 <tbody>
                                 <tr>
-                                    <td>2</td>
-                                    <td>$ 1,300</td>
+                                    <td>{$sub_db2['g_entry']}</td>
+                                    <td>{$sub_db3['left_salary']}</td>
                                 </tr>
                                 </tbody>
                             </table>
@@ -123,17 +195,8 @@ try {
                                 </thead>
                                 <tbody>
 LI;
-                            $sub_query  = "
-                                SELECT * FROM lineups a
-                                LEFT JOIN game b 
-                                    ON b.g_idx = a.lu_g_idx
-                                LEFT JOIN lineups_history c 
-                                    ON c.lu_idx = a.lu_idx
-                                WHERE 1=1
-                                    AND c.lu_idx = {$db['lu_idx']}
-                            ";
-                            //p($sub_query);
-                            $sub_result = $_mysqli->query($sub_query);
+
+
                             if (!$sub_result) {
                             }
                             while ($sub_db = $sub_result->fetch_assoc()) {
@@ -155,7 +218,7 @@ TR;
                                             <ul>
                                                 <li><p>기대 상금</p><span>702</span></li>
                                                 <li><p>총 상금</p><span class="fc-yellow">702</span></li>
-                                                <li><p>수정 시간</p><span>2022-10-27 12:02:30</span></li>
+                                                <li><p>수정 시간</p><span><?=$sub_db3['g_date']?></span></li>
                                             </ul>
                                         </div>
                                     </td>
@@ -165,16 +228,22 @@ TR;
                         </li>
                         <?php
                         }
+                        }
+                        $result->free();
+                        $sub_result->free();
                         ?>
                     </ul>
                 </div>
             </section>
             <!--//sec-01-->
             <div class="pagination">
-                <a href="javascript:void(0)" class="active" >1</a>
+                <?php
+                echo paging($page,$total_page,5,"{$_SERVER['SCRIPT_NAME']}?page=");
+                ?>
+                <!--<a href="javascript:void(0)" class="active" >1</a>
                 <a href="javascript:void(0)">2</a>
                 <a href="javascript:void(0)">3</a>
-                <a href="javascript:void(0)">4</a>
+                <a href="javascript:void(0)">4</a>-->
             </div>
         </div>
         <!--//content-->
