0036a16d45b2d959ef5cf67ad7ac3ab4f4cf8e59
오태일 <tioh@idevel.kr>
2022-06-22 16:09:30 +0900

    레드마인 #1653 head 부분 구글어낼리틱스 추가

--- www/common/head.php
+++ www/common/head.php
@@ -15,4 +15,14 @@
 <script src="/js/common.js?v=<?=JSYYYYMMDD;?>"></script>
 <script src="/js/dev.js?v=<?=JSYYYYMMDD;?>"></script>
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-8R5GL9W2FE"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-8R5GL9W2FE');
+</script>
+
 <title>METAGAMES</title>
