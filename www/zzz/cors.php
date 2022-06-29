63c0765ab989f99d779ed4b255565fd1fc7f0892
진경수 <gsjin@idevel.co.kr>
2022-06-30 08:01:11 +0900

    cors 테스트 페이지

---
+++ www/zzz/cors.php
@@ -0,0 +1,14 @@
+<html>
+<head></head>
+<body>
+<form method="post" action="http://dev.2winchance.com/api/setNotice.php">
+    <input name="mode" type="text" value="c"/>
+    <input name="seq" type="text" value="0"/>
+    <input name="title" type="text" value="제목입니다."/>
+    <input name="content" type="text" value="내용입니다."/>
+    <input name="show_yn" type="text" value="Y"/>
+    <input name="reg_nm" type="text" value="진경수"/>
+    <button>전송</button>
+</form>
+</body>
+</html>
